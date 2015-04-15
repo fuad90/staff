8ec59a1a63f34116cf8f5a29dd131458c971eae1
fuad90 <CDSB@pokoks-MacBook-Air.local>
2015-04-15 11:09:22 +0800

    change

--- www/js/json.js
+++ www/js/json.js
@@ -1,7 +1,9 @@
 angular.module('starter.json', [])
 
 .factory('dataAll', function($http) {
-    var baseUrl = 'http://localhost/api/web/index.php/staff/';
+    //var baseUrl = 'http://localhost/api/web/index.php/staff/';
+    var baseUrl = 'http://192.168.0.128/api/web/index.php/staff/';
+   
     return {
         getAll: function() {
             return $http.get(baseUrl+'index');
